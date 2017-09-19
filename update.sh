d0ac0ea0807034969fb89d920b16110507b2969f
Luís Ferreira <lsferreira169@gmail.com>
2017-09-19 22:25:40 +0100

    Update docker tag

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@ wget http://os.archlinuxarm.org/os/ArchLinuxARM-armv7-latest.tar.gz
 mkdir -p system
 bsdtar -xpf ArchLinuxARM-armv7-latest.tar.gz -C system
 rm ArchLinuxARM-armv7-latest.tar.gz
-docker build -t archlinux-armhf .
+docker build -t aurorafw/archlinux-armhf .
 rm -rf system
